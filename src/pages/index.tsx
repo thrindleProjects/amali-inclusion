23c25922f95acbb9f85ae827675097d008069df2
khallekan <okeolalekanisaac@gmail.com>
2022-12-06 17:25:30 +0100

    Fix linting errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,13 +13,12 @@ export default function Home() {
 			</Head>
 
 			<main className={styles.main}>
-				<h1 className='text-red-600'>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+				<h1 className="text-red-600">
+					Welcome to <a href="https://nextjs.org">Next.js!</a>
 				</h1>
 
 				<p className={styles.description}>
-          Get started by editing{" "}
-					<code className={styles.code}>pages/index.tsx</code>
+					Get started by editing <code className={styles.code}>pages/index.tsx</code>
 				</p>
 
 				<div className={styles.grid}>
@@ -29,15 +28,11 @@ export default function Home() {
 					</a>
 
 					<a href="https://nextjs.org/learn" className={styles.card}>
-
 						<h2>Learn &rarr;</h2>
 						<p>Learn about Next.js in an interactive course with quizzes!</p>
 					</a>
 
-					<a
-						href="https://github.com/vercel/next.js/tree/canary/examples"
-						className={styles.card}
-					>
+					<a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
 						<h2>Examples &rarr;</h2>
 						<p>Discover and deploy boilerplate example Next.js projects.</p>
 					</a>
@@ -49,9 +44,7 @@ export default function Home() {
 						className={styles.card}
 					>
 						<h2>Deploy &rarr;</h2>
-						<p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-						</p>
+						<p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
 					</a>
 				</div>
 			</main>
@@ -62,7 +55,7 @@ export default function Home() {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-          Powered by{" "}
+					Powered by{" "}
 					<span className={styles.logo}>
 						<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
 					</span>
