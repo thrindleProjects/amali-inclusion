d5455fa991c9577dfc729cca3a60a29fb568672a
mightyfzeus <bolarinolabisi36@gmail.com>
2022-12-08 15:52:32 +0100

    agents and dashboard

--- src/components/lib/Input/Input.tsx
+++ src/components/lib/Input/Input.tsx
@@ -6,7 +6,7 @@ const DateInput: React.FC<DateInputProps> = ({ label, type, placeholder }) => {
 		<div className="mt-4 w-full">
 			<p className='text-sm mb-2'>{label}</p>
 
-			<div className=" border border-grey-300 h-auto rounded-md   px-3 py-2 ">
+			<div className=" border border-grey-300 h-auto rounded-md   px-3 py-2 focus:border-amali-green ">
 				<input type={type} placeholder={placeholder} className='w-full h-full outline-none' />
 			</div>
 		</div>
